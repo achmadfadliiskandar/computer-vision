14db9bbabed7e9208cc3d185b8552bc856cbe1a2
achmadfadliiskandar <af137357@gmail.com>
2025-03-15 15:56:04 +0700

    adding study case new

---
+++ absensi.py
@@ -0,0 +1,22 @@
+import getpass
+print("Absensi Python")
+
+datakaryawan = {
+    "Nabil": {"NPM": "51422186", "kelas": "3IA13"},
+    "Fadli": {"NPM": "50422069", "kelas": "3IA13"},
+    "Adrian": {"NPM": "50422102", "kelas": "3IA13"}
+}
+
+# Contoh akses data
+# print(datakaryawan['Fadli'])
+
+# masukin input
+dataisi = str(getpass.getpass(prompt='Masukan Data Karyawan : '))
+found = False
+
+for key,value in datakaryawan.items():
+    if dataisi in value.values():
+        print(key,value)
+        found = True
+if not found:
+    print("tidak ada")
