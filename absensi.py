3853666fc8d5a9a4efdcdcb3128b70139a5593e6
achmadfadliiskandar <af137357@gmail.com>
2025-03-22 14:05:30 +0700

    try to use txt for storage data

--- absensi.py
+++ absensi.py
@@ -2,12 +2,15 @@ import getpass
 print("Absensi Python")
 
 datakaryawan = {
-    "Nabil": {"NPM": "51422186", "kelas": "3IA13"},
-    "Fadli": {"NPM": "50422069", "kelas": "3IA13"},
-    "Adrian": {"NPM": "50422102", "kelas": "3IA13"},
-    "Wisnu": {"NPM": "51422635", "kelas": "3IA19"},
+    "Nabil": {"NPM": "51422186", "kelas": "3IA13","status":"tidak_hadir"},
+    "Fadli": {"NPM": "50422069", "kelas": "3IA13","status":"tidak_hadir"},
+    "Adrian": {"NPM": "50422102", "kelas": "3IA13","status":"tidak_hadir"},
+    "Wisnu": {"NPM": "51422635", "kelas": "3IA19","status":"tidak_hadir"},
 }
 
+filename = "data.txt"
+with open(filename,"r") as file:
+    lines = file.readlines()
 # Contoh akses data
 # print(datakaryawan['50422069'])
 
@@ -17,7 +20,13 @@ found = False
 
 for key,value in datakaryawan.items():
     if dataisi in value.values():
-        print(key,value)
+        print(f"status {key} hadir")
+        with open(filename, "w") as file:
+            for line in lines:
+                if dataisi in line:
+                    line = line.replace("tidak_hadir", "hadir")  # Ubah status
+                file.write(line)
         found = True
+# jika tidak ada input data dari user
 if not found:
     print("tidak ada")
